a2689675b2e6f94cd59b2f66e78abd34c74d144b
Saif Ahmed <saifmdco@gmail.com>
2020-01-02 13:00:37 +0530

    only admin is allowed

--- leave_tracker/leave_sys/views.py
+++ leave_tracker/leave_sys/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets
 from .models import LeaveType, Leave, Staff, Holiday
 from .serializers import LeaveSerializer, LeaveTypeSerializer, StaffSerializer, HolidaySerializer
 from django_filters.rest_framework import DjangoFilterBackend
-
+from rest_framework.permissions import IsAdminUser
 
 
 class StaffViewset(viewsets.ModelViewSet):
@@ -11,20 +11,25 @@ class StaffViewset(viewsets.ModelViewSet):
     serializer_class = StaffSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['staff',]
+    permission_classes = [IsAdminUser]
+
 
 class LeaveViewset(viewsets.ModelViewSet):
     queryset = Leave.objects.all()
     serializer_class = LeaveSerializer
     filterset_fields = ['staff','leave_type', 'from_date', 'to_date', ]
+    permission_classes = [IsAdminUser]
 
 
 class LeaveTypeViewset(viewsets.ModelViewSet):
     queryset = LeaveType.objects.all()
     serializer_class = LeaveTypeSerializer
     filterset_fields = ['leave_type',]
+    permission_classes = [IsAdminUser]
 
 
 class HolidayViewset(viewsets.ModelViewSet):
     queryset = Holiday.objects.all()
     serializer_class = HolidaySerializer
     filterset_fields = ['name', 'from_date', 'to_date']
+    permission_classes = [IsAdminUser]
